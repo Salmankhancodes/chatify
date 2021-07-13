e4f1176b01d33ebbdc09c05205e465600b9d21c8
Salmankhancodes <117salmankhan@gmail.com>
2021-07-14 01:33:35 +0530

    Added authentication system

--- chatify/src/App.js
+++ chatify/src/App.js
@@ -1,10 +1,30 @@
-
 import './App.css';
+import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './components/Login';
+import { useStateValue } from './components/StateProvider';
 
 function App() {
+  const [{user}]=useStateValue()
   return (
-    <div className="App">
+    <div className="app">
+        {!user ? <Login /> :(
+      <div className="app_body">
 
+          <Router>
+          <Sidebar />
+          <Switch>
+            <Route path="/rooms/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <Chat />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+          )}
     </div>
   );
 }
---
+++ chatify/src/components/Login.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Button } from '@material-ui/core'
+import { auth, provider } from '../firebase'
+import './Login.css'
+import { actionTypes } from './reducer'
+import { useStateValue } from './StateProvider'
+
+
+
+
+const Login = () => {
+    const [{},dispatch]=useStateValue()
+    const signIn = () => {
+        
+        auth.signInWithPopup(provider)
+        .then(result => {
+            dispatch({
+                type:actionTypes.SET_USER,
+                user:result.user
+            })
+        })
+        .catch(error => alert(error.message))
+        
+    }
+
+    return (
+        <div className="login">
+            <div className="login_container">
+                <img src="" alt="" />
+                <div className="login_text">
+                    <h1>Sign in to whatsapp</h1>
+                </div>
+                <Button onClick={signIn}>Sign in with Google</Button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Login
